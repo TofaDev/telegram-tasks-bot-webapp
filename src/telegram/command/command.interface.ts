938a05dec60190797aa7ee2186952c6f90c80bf5
TofaDev <xnotasunax@gmail.com>
2024-10-11 19:13:16 +0700

    update

--- src/telegram/command/command.interface.ts
+++
@@ -1,9 +0,0 @@
-import { UserRole } from "../../enum/UserRole"
-import { BotContext } from "../BotContext.interface"
-
-export interface ICommand {
-    name: string
-    desk: string
-    role: UserRole
-    execute(context: BotContext): Promise<void>
-}
--- src/telegram/listener/MessageListener.ts
+++
@@ -1,8 +0,0 @@
-import {BotContext} from "../BotContext.interface";
-
-export class MessageListener {
-
-    async listen(context: BotContext): Promise<void> {
-        const {id, registrationDate, role, telegramUid} = context.viewer!         
-    }
-}
